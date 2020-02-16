e832854997652e29ec72b415d00fced39fa62f67
Tekill39 <minit009@yandex.ru>
2020-02-16 12:05:19 +0200

    team

--- js/main.js
+++ js/main.js
@@ -18,5 +18,13 @@ $(document).ready(function() {
             var filter=$(this).attr('filter');
             $('.filter > div[filter='+filter+']').show(300);
          }
-    })           
+    })  
+    $('.multiple-items').slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      arrows:false,
+      dots:true,
+      dotsClass:'dots-style'
+    });         
 })
