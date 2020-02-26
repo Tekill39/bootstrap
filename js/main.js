c980d2caaf31e79389155b1c240479bc8e157232
Tekill39 <minit009@yandex.ru>
2020-02-26 09:40:34 +0200

    1

--- js/main.js
+++ js/main.js
@@ -25,6 +25,34 @@ $(document).ready(function() {
       slidesToScroll: 3,
       arrows:false,
       dots:true,
-      dotsClass:'dots-style'
+      dotsClass:'dots-style',
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
     });         
 })
